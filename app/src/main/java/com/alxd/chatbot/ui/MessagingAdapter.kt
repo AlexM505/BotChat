4da35d19ce1cdd514aceec09e461eea6dc6a4eed
AlxD <alexandermarenco4@gmail.com>
2021-06-16 15:38:38 -0600

    Validando ID del mensaje para saber cual texto mostrar RECIBIDO o ENVIADO

--- app/src/main/java/com/alxd/chatbot/ui/MessagingAdapter.kt
+++ app/src/main/java/com/alxd/chatbot/ui/MessagingAdapter.kt
@@ -5,11 +5,23 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.alxd.chatbot.R
+import com.alxd.chatbot.data.Message
+import com.alxd.chatbot.utils.Constants.RECEIVE_ID
+import com.alxd.chatbot.utils.Constants.SEND_ID
+import kotlinx.android.synthetic.main.message_itm.view.*
 
 class MessagingAdapter : RecyclerView.Adapter<MessagingAdapter.MessageViewHolder>() {
 
-    inner class MessageViewHolder(itemView : View) : RecyclerView.ViewHolder(itemView){
+    var messageList = mutableListOf<Message>()
 
+    inner class MessageViewHolder(itemView : View) : RecyclerView.ViewHolder(itemView){
+        init {
+            itemView.setOnLongClickListener {
+                messageList.removeAt(adapterPosition)
+                notifyItemRemoved(adapterPosition)
+                true
+            }
+        }
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MessageViewHolder {
@@ -20,10 +32,32 @@ class MessagingAdapter : RecyclerView.Adapter<MessagingAdapter.MessageViewHolder
     }
 
     override fun onBindViewHolder(holder: MessageViewHolder, position: Int) {
-        TODO("Not yet implemented")
+        val currentMessage = messageList[position]
+
+        when(currentMessage.id){
+            SEND_ID -> {
+                holder.itemView.tv_message.apply {
+                    text = currentMessage.message
+                    visibility = View.VISIBLE
+                }
+                holder.itemView.tv_bot_message.visibility = View.GONE
+            }
+            RECEIVE_ID -> {
+                holder.itemView.tv_bot_message.apply {
+                    text = currentMessage.message
+                    visibility = View.VISIBLE
+                }
+                holder.itemView.tv_message.visibility = View.GONE
+            }
+        }
     }
 
     override fun getItemCount(): Int {
-        TODO("Not yet implemented")
+        return messageList.size
+    }
+
+    fun insertMessage(message: Message){
+        this.messageList.add(message)
+        notifyItemInserted(messageList.size)
     }
 }
